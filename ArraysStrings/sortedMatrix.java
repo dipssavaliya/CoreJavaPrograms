184363060eeaab88851abd2ec260780c7ed7a053
Dipali <savaliyadipali13@gmail.com>
2023-10-26 08:27:11 -0400

    staircaseSearch
    Signed-off-by: Dipali <savaliyadipali13@gmail.com>

---
+++ ArraysStrings/sortedMatrix.java
@@ -0,0 +1,38 @@
+package ArraysStrings;
+public class sortedMatrix {
+  public static void main(String[] args) {
+    int[][] matrix=
+    {
+      {1,2,3,4},
+      {5,6,7,8},
+      {9,10,11,12},
+      {13,14,15,16}
+    };
+        int key=13;
+        System.out.println(serchElementMethod(matrix,key));
+  }
+  public static boolean serchElementMethod(int[][] matrix,int key)
+  {
+    int row=0; int col=matrix[0].length-1;
+    while(row<matrix.length && col>=0)
+    {
+      if(matrix[row][col]==key)
+      {
+        System.out.println("found key at ( "+row +" " + col +")");
+        return true;
+      }
+      //left
+      else if(key<matrix[row][col])
+      {
+        col--;
+      }
+      //bottom
+      else 
+      {
+        row++;
+      }
+    }
+    System.out.println("key not found!");
+    return false;
+  }
+}
