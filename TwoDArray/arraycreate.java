acd138a35437d9d276c7739485e82e603959f661
Dipali <savaliyadipali13@gmail.com>
2024-07-05 14:59:33 -0400

    2d array
    Signed-off-by: Dipali <savaliyadipali13@gmail.com>

---
+++ TwoDArray/arraycreate.java
@@ -0,0 +1,34 @@
+package TwoDArray;
+import java.util.Scanner;
+
+public class arraycreate {
+  public static void main(String[] args)
+  {
+    int[][] matrix = new int[3][3];
+    arraycreate ac = new arraycreate();
+    ac.arraycreat(matrix);
+    ac.arraydisplay(matrix);
+  }
+  public  void arraycreat(int[][] matrix)
+  {
+    Scanner sc = new Scanner(System.in);
+    for(int i=0;i<matrix.length;i++)
+    {
+      for(int j=0;j<matrix[0].length;j++){
+    
+      matrix[i][j]=sc.nextInt();
+      }
+    }
+  }
+  
+  public  void arraydisplay(int[][] matrix)
+  {
+    for(int i=0;i<matrix.length;i++)
+    {
+      for(int j=0;j<matrix[0].length;j++){
+      System.out.print(matrix[i][j]);
+      }
+      System.out.println();
+    } 
+  }
+}
